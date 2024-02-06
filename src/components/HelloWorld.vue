e3b05b7c13a6d0573fb1ebc899c61b456d68a4dc
alijuniors <75438240+alijuniors@users.noreply.github.com>
2024-02-06 12:30:14 -0400

    creo repositorio proyecto nuevo

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,29 +1,41 @@
-<template>
-  <h1>{{model1.title}}</h1>
-</template>
-
-
-
-<script lang="ts" setup>
-
-import { JsonDemo } from "../models/models_demo";
-
-const model1:JsonDemo = {
-  userId: 100,
-    id: 200,
-    title: "Titulo",
-    completed: false,
-    data:"sd"
-
-}
+<script setup lang="ts">
+defineProps<{
+  msg: string
+}>()
 </script>
 
+<template>
+  <div class="greetings">
+    <h1 class="green">{{ msg }}</h1>
+    <h3>
+      You’ve successfully created a project with
+      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
+      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
+    </h3>
+  </div>
+</template>
 
 <style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
 
-h1{
- 
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
 }
 </style>
-
-
