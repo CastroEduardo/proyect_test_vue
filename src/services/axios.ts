37fe19d59d5f0a3fa6ef650c691d3f40c10f84d7
alijuniors <75438240+alijuniors@users.noreply.github.com>
2024-02-07 15:14:03 -0400

    se crea servicio desde api que consume

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,15 +1,7 @@
-import axios, {AxiosInstance} from 'axios'
+import axios from 'axios'
 import { CorsOrigin } from 'vite'
-const express = require('express');
-const cors = require("cors"); //this is a library to config easily cors
-
-const axiosInstance : AxiosInstance = axios.create({
-    baseURL: 'http://localhost:3000/api',
-    headers:{
-        'Content-Type': 'application/json; charset=utf-8',
-        'Accept': '*/*',
-        'Access-Control-Allow-Origin': 'GET,POST,OPTIONS,DELETE,PUT',
-        'User-Agent': 'Thunder Client (https://www.thunderclient.com)' 
-    }
+const headers ={};
+const axiosInstance =  axios.create({
+    baseURL: 'http://localhost:3000/api/'
 })
 export default axiosInstance;
